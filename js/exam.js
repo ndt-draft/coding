d0c9a46502e9f729e80943376d5d625dbed43690
thanh4890 <thanh4890@gmail.com>
2018-06-19 22:30:57 +0700

    Add click event to select app and update input value with corresponding app name

--- js/exam.js
+++ js/exam.js
@@ -1,15 +1,15 @@
 (function() {
+  let contents = document.getElementsByClassName('contents')[0]
+
   // function to render apps in .contents
   function renderApps(data) {
-    let contents = document.getElementsByClassName('contents')[0]
-
-    let html = '<div class="apps list-group">'
+    let html = '<div class="app-list list-group">'
 
     data.map(function(item) {
-      html += '<li class="app list-group-item">'
+      html += '<div class="app list-group-item">'
         html += '<img class="app-thumbnail" src="' + item.thumbnailUrl + '">'
         html += item.name
-      html += '</li>'
+      html += '</div>'
     })
 
     html += '</div>'
@@ -33,6 +33,38 @@
   // add event listener to input
   let input = document.getElementsByName('apps')[0]
   input.addEventListener('input', suggestAppsByKeyword)
+
+  let activeAppIndex
+  // function to select app
+  function selectApp(e) {
+    let apps = contents.childNodes[0].childNodes
+    if(e.target && e.target.nodeName == "DIV") {
+      // remove active class from old active app
+      if (activeAppIndex !== undefined) {
+        apps[activeAppIndex].classList.remove('active-app')
+      }
+
+      // add active class to current target
+      e.target.classList.add('active-app')
+
+      // update input field value with corresponding app name
+      input.value = e.target.textContent
+
+      // find app index
+      // https://stackoverflow.com/a/5913984
+      let targetIndex = 0;
+      let target = e.target
+      while( (target = target.previousSibling) != null ) {
+        targetIndex++;
+      }
+
+      // save active app index
+      activeAppIndex = targetIndex
+    }
+  }
+
+  // add click event to each app
+  contents.addEventListener('click', selectApp)
 
   // run
   renderApps(TABLE_DATA)
