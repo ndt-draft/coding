7b3a886f563d85a5d08ff8968b6b48bc1f61d784
thanh4890 <thanh4890@gmail.com>
2018-06-24 23:09:58 +0700

    Add more apps

--- json/data.js
+++ json/data.js
@@ -48,5 +48,55 @@ var TABLE_DATA = [
     "id": "7",
     "name": "Tips",
     "thumbnailUrl": "image/tips.png"
-  }
+  },
+  {
+    "id": "11",
+    "name": "Skype",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/skype-chat-messenger-media-social_icon-icons.com_59297.png"
+  },
+  {
+    "id": "12",
+    "name": "Google Drive",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/google-drive-triangle-brand-storage_icon-icons.com_59277.png"
+  },
+  {
+    "id": "13",
+    "name": "YouTube",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/youtube-video-social-media-play_icon-icons.com_59261.png"
+  },
+  {
+    "id": "14",
+    "name": "WhatsApp",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/whatsapp-phone-circle-shape-brand_icon-icons.com_59271.png"
+  },
+  {
+    "id": "15",
+    "name": "Twitter",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/twitter-bird-animal-social-media_icon-icons.com_59258.png"
+  },
+  {
+    "id": "16",
+    "name": "Instagram",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/instagram-social-media-camera-photo_icon-icons.com_59255.png"
+  },
+  {
+    "id": "17",
+    "name": "Blogger",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/blogger-brand-social-media-blog-b_icon-icons.com_59298.png"
+  },
+  {
+    "id": "18",
+    "name": "Vimeo",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/vimeo-video-social-media-letter-v_icon-icons.com_59314.png"
+  },
+  {
+    "id": "19",
+    "name": "Spotify",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/circle-round-shape-signal-bar_icon-icons.com_59320.png"
+  },
+  {
+    "id": "20",
+    "name": "Twitch",
+    "thumbnailUrl": "https://cdn.icon-icons.com/icons2/643/PNG/128/chat-box-pause-equal-shape_icon-icons.com_59321.png"
+  },
 ]
